639ee40fbec61f7b55d9d35055b92a8bdb989fb6
Jerry GIraffe <jerrythegiraffe007@gmail.com>
2019-06-13 16:28:15 -0400

    addMorgan

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = (process.env.PORT||3000);
 
+const morgan = require("morgan");
+app.use(morgan("short"));
+
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
